f368dbe5ab99a5d9df08cc817a9fc7e9266276b3
Takayyyy <j1636tm@gmail.com>
2019-12-01 08:37:09 +0000

    Add ALL

--- main.rb
+++ main.rb
@@ -4,35 +4,48 @@ include DXOpal
 
 require_remote 'player.rb'
 require_remote 'enemy.rb'
+require_remote 'wall.rb'
 
 Image.register(:player, 'images/player.png') 
 Image.register(:enemy, 'images/enemy.png') 
 
+Image.register(:wall1,'images/wall1.png')
+Image.register(:wall2,'images/wall2.png')
+Image.register(:floor,'images/floor_fg.png')
+
 Window.load_resources do
-  Window.width  = 800
-  Window.height = 600
+  Window.width  = 1200
+  Window.height = 800
 
   player_img = Image[:player]
   player_img.set_color_key([0, 0, 0])
 
   enemy_img = Image[:enemy]
   enemy_img.set_color_key([0, 0, 0])
-
+  
+  wall_img1 = Image[:wall1] 
+  wall_img2 = Image[:wall2]
+  
+  wall_img1.set_color_key([255, 255, 255])
+  wall_img2.set_color_key([255, 255, 255])
+  
+  wall_imgs = [wall_img1,wall_img2]
+  
+  floor = Image[:floor]
+  
   player = Player.new(400, 500, player_img)
+  wall =PassageWayTypeA.new(wall_imgs,300)
 
-  enemies = []
-  10.times do
-    enemies << Enemy.new(rand(800), rand(600), enemy_img)
-  end
 
   Window.loop do
-    Sprite.update(enemies)
-    Sprite.draw(enemies)
-
-    player.update
-    player.draw
+    #Sprite.update(enemies)
+    #Sprite.draw(enemies)
+     wall.update(0, 0, -25)
+     wall.draw(0, 0)
+ 
+     Window.draw(0, 0, floor)
 
     # 当たり判定
-    Sprite.check(player, enemies)
+    #Sprite.check(player, enemies)
   end
 end
